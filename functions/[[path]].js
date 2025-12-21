ce64849377d166979b796d0f8cda2b5a69a8cbae
sarthaksingh7506-blip <sarthaksingh7506@gmail.com>
2025-12-21 20:42:25 +0530

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -2,24 +2,10 @@ export async function onRequest({ request, env }) {
   const url = new URL(request.url);
   const pathname = url.pathname;
 
-  if (pathname.endsWith("/reply/templateList")) {
+  // match /reply/templateList AND /reply/templateList/ AND with params
+  if (pathname.startsWith("/reply/templateList")) {
     return indexReplyTemplates(env);
   }
 
-  // Let Cloudflare Pages serve static files automatically
   return env.ASSETS.fetch(request);
-}
-
-async function indexReplyTemplates(env) {
-  const manifest = env.__STATIC_CONTENT_MANIFEST;
-
-  const files = JSON.parse(manifest);
-
-  const list = Object.keys(files)
-    .filter(p => p.startsWith("reply/") && p.endsWith(".html"))
-    .map(p => p.replace("reply/", "").replace(".html", ""));
-
-  return new Response(JSON.stringify(list, null, 2), {
-    headers: { "Content-Type": "application/json" }
-  });
 }
