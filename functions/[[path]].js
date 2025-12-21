63badcaa4dd1bd5d63c3ae064e7418104bcd0010
sarthaksingh7506-blip <sarthaksingh7506@gmail.com>
2025-12-21 20:30:31 +0530

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,22 +1,23 @@
-export const onRequest = async ({ request, env, params }) => {
-  const path = params.path || "";
+export async function onRequest({ request, env, params }) {
+  const url = new URL(request.url);
+  const pathname = url.pathname;
 
-  if (path.endsWith("templateList")) {
+  if (pathname.endsWith("/reply/templateList")) {
     return indexReplyTemplates(env);
   }
 
   return env.ASSETS.fetch(request);
-};
+}
 
 async function indexReplyTemplates(env) {
-  const manifest = __STATIC_CONTENT_MANIFEST;
+  const manifest = env.__STATIC_CONTENT_MANIFEST;
   const files = JSON.parse(manifest);
 
-  const templateList = Object.keys(files)
+  const list = Object.keys(files)
     .filter(p => p.startsWith("reply/") && p.endsWith(".html"))
     .map(p => p.replace("reply/", "").replace(".html", ""));
 
-  return new Response(JSON.stringify(templateList, null, 2), {
+  return new Response(JSON.stringify(list, null, 2), {
     headers: { "Content-Type": "application/json" }
   });
 }
