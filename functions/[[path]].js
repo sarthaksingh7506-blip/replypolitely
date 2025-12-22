ff9e2d723d18bac63417d568dffb1acc92983667
sarthaksingh7506-blip <sarthaksingh7506@gmail.com>
2025-12-22 16:35:51 +0530

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -2,10 +2,22 @@ export async function onRequest({ request, env }) {
   const url = new URL(request.url);
   const pathname = url.pathname;
 
-  // match /reply/templateList AND /reply/templateList/ AND with params
   if (pathname.startsWith("/reply/templateList")) {
     return indexReplyTemplates(env);
   }
 
   return env.ASSETS.fetch(request);
+}
+
+async function indexReplyTemplates(env) {
+  const manifest = env.__STATIC_CONTENT_MANIFEST;
+  const files = JSON.parse(manifest);
+
+  const list = Object.keys(files)
+    .filter(p => p.startsWith("reply/") && p.endsWith(".html"))
+    .map(p => p.replace("reply/", "").replace(".html", ""));
+
+  return new Response(JSON.stringify(list, null, 2), {
+    headers: { "Content-Type": "application/json" }
+  });
 }
